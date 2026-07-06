8d4577669cfa280724495a176f28e6b1a9725c8b
lexih <git@lexih.dev>
2026-07-06 03:08:18 +0200

    Expose Sunshine startup options

--- module/software/sunshine/default.nix
+++ module/software/sunshine/default.nix
@@ -8,14 +8,24 @@
 in {
   options.modules.software.sunshine = {
     enable = lib.mkEnableOption "Sunshine game streaming server";
+    autoStart = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Start Sunshine automatically at boot.";
+    };
+    openFirewall = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Open firewall ports for Sunshine.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     services.sunshine = {
       enable = true;
-      autoStart = false;
+      autoStart = cfg.autoStart;
       capSysAdmin = true;
-      openFirewall = true;
+      openFirewall = cfg.openFirewall;
     };
 
     boot.kernelModules = ["uinput"];
