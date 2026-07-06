4376027147bd92aeb7d040e6d3fa90ae00f347f5
lexih <git@lexih.dev>
2026-07-06 03:05:31 +0200

    Make Docker insecure registries configurable

--- module/software/docker/default.nix
+++ module/software/docker/default.nix
@@ -8,13 +8,18 @@ in {
   options.modules.software.docker = {
     enable = lib.mkEnableOption "Docker container runtime";
     nvidia.enable = lib.mkEnableOption "NVIDIA container toolkit";
+    insecureRegistries = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = ["localhost:5000"];
+      description = "Docker insecure registries for local development.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     virtualisation.docker = {
       enable = true;
       enableOnBoot = false; # start on demand
-      daemon.settings.insecure-registries = ["localhost:5000"];
+      daemon.settings.insecure-registries = cfg.insecureRegistries;
     };
 
     hardware.nvidia-container-toolkit.enable = cfg.nvidia.enable;
