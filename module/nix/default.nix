0874884ca6e583150a9a3bb99c7b1d357b4ac349
lexih <git@lexih.dev>
2026-07-06 03:06:25 +0200

    Make insecure package allowlist configurable

--- module/nix/default.nix
+++ module/nix/default.nix
@@ -1,40 +1,55 @@
-{inputs, ...}: {
-  environment.etc."nix/inputs/nixpkgs".source = inputs.nixpkgs;
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "pnpm-10.29.2"
-  ];
-
-  nix = {
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = ["nix-command" "flakes"];
-      warn-dirty = false;
-      substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://attic.xuyh0120.win/lantian"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-      ];
-      max-jobs = "auto";
-      cores = 0;
-      use-xdg-base-directories = true; # respect xdg dirs
-      download-buffer-size = 524288000; # workaround for nix#11728
-      trusted-users = ["root" "@wheel"];
-    };
-
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-      persistent = true; # run missed gc after sleep
-    };
+{
+  config,
+  inputs,
+  lib,
+  ...
+}: let
+  cfg = config.modules.nix;
+in {
+  options.modules.nix.permittedInsecurePackages = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    default = [
+      "pnpm-10.29.2"
+    ];
+    description = "Insecure nixpkgs packages allowed for this system.";
   };
 
-  programs.nix-index-database.comma.enable = true;
-  programs.nix-ld.enable = true;
+  config = {
+    environment.etc."nix/inputs/nixpkgs".source = inputs.nixpkgs;
+
+    nixpkgs.config.permittedInsecurePackages = cfg.permittedInsecurePackages;
+
+    nix = {
+      settings = {
+        auto-optimise-store = true;
+        experimental-features = ["nix-command" "flakes"];
+        warn-dirty = false;
+        substituters = [
+          "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
+          "https://attic.xuyh0120.win/lantian"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+        ];
+        max-jobs = "auto";
+        cores = 0;
+        use-xdg-base-directories = true; # respect xdg dirs
+        download-buffer-size = 524288000; # workaround for nix#11728
+        trusted-users = ["root" "@wheel"];
+      };
+
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+        persistent = true; # run missed gc after sleep
+      };
+    };
+
+    programs.nix-index-database.comma.enable = true;
+    programs.nix-ld.enable = true;
+  };
 }
